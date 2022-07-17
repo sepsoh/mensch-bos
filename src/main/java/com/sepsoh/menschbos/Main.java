2973decb06e05ababa728179a587c82b4b2b6485
Sepehr <sepehr7160@gmail.com>
2022-07-18 00:05:04 +0430

    add name of game

--- src/main/java/com/sepsoh/menschbos/Main.java
+++ src/main/java/com/sepsoh/menschbos/Main.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Mensch-Bos");
         stage.setScene(scene);
         stage.show();
     }

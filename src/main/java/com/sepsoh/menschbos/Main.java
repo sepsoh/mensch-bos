e5a9a2f903235a54dcc6bbdad76b8abdea351a8c
Sepehr <sepehr7160@gmail.com>
2022-07-18 13:35:12 +0430

    remove unnecessary imports

--- src/main/java/com/sepsoh/menschbos/Main.java
+++ src/main/java/com/sepsoh/menschbos/Main.java
@@ -5,11 +5,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import org.kordamp.bootstrapfx.BootstrapFX;
-
 import java.io.IOException;
-import java.util.Map;
 
 public class Main extends Application {
 

58c22ea5113582b1d8646d168a6e1e6177bf5812
Sepehr <sepehr7160@gmail.com>
2022-07-18 13:28:03 +0430

    get title of app from AppData

--- src/main/java/com/sepsoh/menschbos/Main.java
+++ src/main/java/com/sepsoh/menschbos/Main.java
@@ -1,25 +1,52 @@
 package com.sepsoh.menschbos;
 
+import com.sepsoh.menschbos.data.AppData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
+import java.util.Map;
 
 public class Main extends Application {
+
+    static Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
+        Main.stage = stage;
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
-        stage.setTitle("Mensch-Bos");
+        stage.setTitle(AppData.appTitle);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
     public static void main(String[] args) {
         launch();
+
     }
+    public static Scene changeScene(String fxmlPath , String title) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource(fxmlPath));
+            Scene scene = new Scene(fxmlLoader.load());
+            Main.stage.setTitle(title);
+            Main.stage.setScene(scene);
+            Main.stage.show();
+            return scene;
+        }catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
+
+    }
+    public static Scene changeScene(String fxmlPath) {
+        return changeScene(fxmlPath,AppData.appTitle);
+
+    }
+
 }

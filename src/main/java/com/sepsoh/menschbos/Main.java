2a680925c6a3cda1b5b4eb905b753f584c9ba57c
Sepehr <sepehr7160@gmail.com>
2022-07-18 11:54:32 +0430

    add bootstrapfx

--- src/main/java/com/sepsoh/menschbos/Main.java
+++ src/main/java/com/sepsoh/menschbos/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 import java.io.IOException;
 
@@ -12,6 +13,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("Mensch-Bos");
         stage.setScene(scene);
         stage.show();

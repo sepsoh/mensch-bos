e6c6e9497f8473dba2e354686470bc2199b30a06
Sepehr <sepehr7160@gmail.com>
2022-07-18 13:29:15 +0430

    get loading image from files in runtime

--- src/main/java/com/sepsoh/menschbos/MenuController.java
+++ src/main/java/com/sepsoh/menschbos/MenuController.java
@@ -1,8 +1,15 @@
 package com.sepsoh.menschbos;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.io.FileInputStream;
 
 public class MenuController {
     @FXML
@@ -11,7 +18,20 @@ public class MenuController {
     @FXML
     public void playOnline(){
 
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(4), new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                loadingImageView.setVisible(false);
+
+            }
+        }));
+        loadingImageView.setImage(new Image(Main.class.getResource("image/loading.gif").toString()));
         loadingImageView.setVisible(true);
+
+        timeline.setCycleCount(1);
+        timeline.play();
+
     }
     @FXML
     public void playWithBots(){
@@ -20,8 +40,7 @@ public class MenuController {
     }
     @FXML
     public void exit(){
-
-
+        Main.stage.close();
     }
 
 

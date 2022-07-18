c227c91a55e40086f0747645c8cd65f1a7be45cd
Sepehr <sepehr7160@gmail.com>
2022-07-18 11:55:56 +0430

    add elements to controller

--- src/main/java/com/sepsoh/menschbos/MenuController.java
+++ src/main/java/com/sepsoh/menschbos/MenuController.java
@@ -1,4 +1,28 @@
 package com.sepsoh.menschbos;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
 public class MenuController {
+    @FXML
+    ImageView loadingImageView;
+
+    @FXML
+    public void playOnline(){
+
+        loadingImageView.setVisible(true);
+    }
+    @FXML
+    public void playWithBots(){
+
+        loadingImageView.setVisible(true);
+    }
+    @FXML
+    public void exit(){
+
+
+    }
+
+
 }

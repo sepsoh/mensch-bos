bfaa2628f036bdb54b06fb653affa8a8d51a4a6c
Sepehr <sepehr7160@gmail.com>
2022-07-30 11:34:01 +0430

    remove all unnecessary Game class method calls

--- src/main/java/com/sepsoh/menschbos/playwithbots/FourPlayerBoardController.java
+++ src/main/java/com/sepsoh/menschbos/playwithbots/FourPlayerBoardController.java
@@ -3,32 +3,45 @@ package com.sepsoh.menschbos.playwithbots;
 
 import com.sepsoh.menschbos.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class FourPlayerBoardController {
+public class FourPlayerBoardController implements Board {
 
 
     @FXML
     ImageView box0,box1,box2,box3,box4,box5;
+    @FXML
+    Label helperLabel;
 
 
     public void initialize(){
         Game game = new Game();
-
-        game.addBox(box0);
-        game.addBox(box1);
-        game.addBox(box2);
-        game.addBox(box3);
-        game.addBox(box4);
-        game.addBox(box5);
-
+        game.init(this);
         game.start();
+
 
     }
 
 
+    @Override
+    public ArrayList<ImageView> getBoxes() {
+        ArrayList<ImageView> boxes = new ArrayList<>();
+        boxes.add(box0);
+        boxes.add(box1);
+        boxes.add(box2);
+        boxes.add(box3);
+        boxes.add(box4);
+        boxes.add(box5);
+        return boxes;
+    }
+
+    @Override
+    public Label getHelperLabel() {
+        return helperLabel;
+    }
 }

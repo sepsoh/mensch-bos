ad623c083a3f24a057fea77a37680c92a10f9480
Sepehr <sepehr7160@gmail.com>
2022-08-01 15:19:22 +0430

    add insertChars(),changeCharPosition() change timeline cycle count to -1

--- src/main/java/com/sepsoh/menschbos/playwithbots/Game.java
+++ src/main/java/com/sepsoh/menschbos/playwithbots/Game.java
@@ -3,14 +3,13 @@ package com.sepsoh.menschbos.playwithbots;
 import com.sepsoh.menschbos.Main;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.geometry.HPos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -19,15 +18,52 @@ public class Game {
     boolean isGameInitialized = false;
     public Timeline mainTimeline = new Timeline();
     private Board board;
+    private HashMap<String,ImageView> characters = new HashMap<>();
     public void init(Board board){
         isGameInitialized = true;
         this.board = board;
+        insertChars();
+    }
+
+    public void insertChars(){
+        String blue = "sunglasses";
+        String yellow = "expressionless";
+        String green = "zany";
+        String red = "money";
+
+        characters.put("blueChar1",getChar(blue));
+        characters.put("blueChar2",getChar(blue));
+        characters.put("blueChar3",getChar(blue));
+        characters.put("blueChar4",getChar(blue));
+
+        characters.put("yellowChar1",getChar(yellow));
+        characters.put("yellowChar2",getChar(yellow));
+        characters.put("yellowChar3",getChar(yellow));
+        characters.put("yellowChar4",getChar(yellow));
+
+        characters.put("greenChar1",getChar(green));
+        characters.put("greenChar2",getChar(green));
+        characters.put("greenChar3",getChar(green));
+        characters.put("greenChar4",getChar(green));
+
+        characters.put("redChar1",getChar(red));
+        characters.put("redChar2",getChar(red));
+        characters.put("redChar3",getChar(red));
+        characters.put("redChar4",getChar(red));
+
+        characters.forEach((s, img) -> {
+            img.setFitHeight(35);
+            img.setFitWidth(35);
+        });
+    }
+    private ImageView getChar(String charName){
+        return new ImageView(new Image(Main.class.getResource("image/character/"+charName+".png").toString()));
     }
 
     public void start(){
         if(isGameInitialized) {
             mainTimeline.getKeyFrames().addAll(new KeyFrame(Duration.seconds(3), event -> next()));
-            mainTimeline.setCycleCount(5);
+            mainTimeline.setCycleCount(-1);
             mainTimeline.play();
         }else{
             System.out.println("you must first call init method");
@@ -38,6 +74,13 @@ public class Game {
     public void hideAllBoxes() {
         board.getBoxes().forEach(box->box.setImage(new Image(Main.class.getResource("image/hide.png").toString())));
 
+    }
+    public void changeCharPosition(String charName,int columnIndex,int rowIndex){
+        ImageView img = characters.get(charName);
+        board.getGridPane().add(img,1,11);
+        board.getGridPane().getChildren().remove(img);
+        board.getGridPane().add(img,2,11);
+        GridPane.setHalignment(img, HPos.CENTER);
     }
     public void changeHelperLabel(){
         if(turn%4 ==1){
@@ -93,7 +136,6 @@ public class Game {
     public void botsTurn(){
         //ImageView box = boxes.get(ThreadLocalRandom.current().nextInt(0, 5 + 1));
         //box.setImage(new Image(Main.class.getResource("image/dice" + dice[boxes.indexOf(box)] + ".png").toString()));
-
 
 
     }

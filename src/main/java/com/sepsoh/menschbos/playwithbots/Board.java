bc0c8537e81e32cbe50e5b1bebfe3deab9264de1
Sepehr <sepehr7160@gmail.com>
2022-08-01 15:16:17 +0430

    add get gridpane

--- src/main/java/com/sepsoh/menschbos/playwithbots/Board.java
+++ src/main/java/com/sepsoh/menschbos/playwithbots/Board.java
@@ -2,10 +2,12 @@ package com.sepsoh.menschbos.playwithbots;
 
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 
 import java.util.ArrayList;
 
 public interface Board {
     ArrayList<ImageView> getBoxes();
     Label getHelperLabel();
+    GridPane getGridPane();
 }

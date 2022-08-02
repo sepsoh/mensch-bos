7733774981e51bcd43c20ea85ec7d459664a81df
Sepehr <sepehr7160@gmail.com>
2022-08-02 14:44:52 +0430

    replace Position instead columnIndex , rowIndex

--- src/main/java/com/sepsoh/menschbos/playwithbots/Character.java
+++ src/main/java/com/sepsoh/menschbos/playwithbots/Character.java
@@ -2,28 +2,33 @@ package com.sepsoh.menschbos.playwithbots;
 
 import javafx.scene.image.ImageView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Character {
-
     private ImageView img;
+    private String charName;
 
-    private int columnIndex;
-    private int rowIndex;
+    private Path path ;
 
-    public Character(ImageView img, int columnIndex, int rowIndex) {
+    public static HashMap<String,Character> characters = new HashMap<>();
+    public Character(String name,String charName,ImageView img) {
         this.img = img;
-        this.columnIndex = columnIndex;
-        this.rowIndex = rowIndex;
+        this.charName = charName;
+        this.path =  new FourPlayerPath(name,characters.size());
+        Character.characters.put(name,this);
     }
-
+    public String getCharName() {
+        return charName;
+    }
     public ImageView getImg() {
         return img;
     }
 
-    public int getColumnIndex() {
-        return columnIndex;
+    public Position getPosition() {
+        return path.getCurrentPosition();
     }
-
-    public int getRowIndex() {
-        return rowIndex;
+    public Position getNextPosition(){
+        return path.getNextPosition();
     }
 }

87516b0e067330d047bc6e28dc3765f460a11f41
Sepehr <sepehr7160@gmail.com>
2022-08-16 15:44:02 +0430

    add getPosition method

--- src/main/java/com/sepsoh/menschbos/playwithbots/Path.java
+++ src/main/java/com/sepsoh/menschbos/playwithbots/Path.java
@@ -1,7 +1,15 @@
 package com.sepsoh.menschbos.playwithbots;
 
+import java.util.ArrayList;
+
 public interface Path {
     Position getCurrentPosition();
+
     Position getNextPosition();
+
+    int getIndex();
+
+    ArrayList<Position> getPosition(int length);
+
 
 }

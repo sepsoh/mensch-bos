99a528d57642268bfdf342f375b04097b708adc5
Sepehr <sepehr7160@gmail.com>
2022-08-18 14:22:33 +0430

    change version to v0.0

--- src/main/java/com/sepsoh/menschbos/data/AppData.java
+++ src/main/java/com/sepsoh/menschbos/data/AppData.java
@@ -3,6 +3,6 @@ package com.sepsoh.menschbos.data;
 import com.sepsoh.menschbos.Main;
 
 public class AppData {
-    public static String appTitle = "Mensch-Bos v1.0";
+    public static String appTitle = "Mensch-Bos v0.0";
     public static String appIcon  = Main.class.getResource("image/character/sunglasses.png").toString();
 }
